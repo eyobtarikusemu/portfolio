ce66d81f063298d388f8dd663620f5463c511aa1
eyobtarikusemu <eyobtariku95@gmail.com>
2025-08-31 17:54:52 +0300

    Update projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getProjects, createProject } = require("../controllers/projects");
+const {
+  getProjects,
+  createProject,
+  getProjectImage,
+} = require("../controllers/projects");
 
 router.get("/projects", getProjects);
 router.post("/projects", createProject);
-//router.get("/projects/:id");
-//router.patch("/projects/:id", (req, res) => {});
-//router.delete("/projects/:id", (req, res) => {});
+router.get("/projects/:projectId/images/:imageIndex", getProjectImage);
 
 module.exports = router;
