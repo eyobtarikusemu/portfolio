8aa02116717c781538e22470dbc46d927fc49601
eyobtarikusemu <eyobtariku95@gmail.com>
2025-08-31 17:55:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const cors = require("cors");
 const router = require("./routes/projects");
 
 app.use(cors());
-app.use(express.json());
-app.use("/bot/uploads", express.static(path.join(__dirname, "/bot/uploads")));
+app.use(express.json({ limit: "50mb" })); // Increase limit for image data
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api", router);
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     startBot();
-    app.listen(3000, () => console.log("Server started"));
+    app.listen(3000, () => console.log("Server started on port 3000"));
   } catch (error) {
     console.log(error);
   }
